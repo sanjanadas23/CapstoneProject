68104beb8ce729645ab8b1a5915089e0725fecde
Sanjana <sanjanad845@gmail.com>
2025-04-15 12:45:15 +0530

    adding actual folder

---
+++ CapstoneProject-LendingApplicationn/src/test/java/com/aurionpro/lending/CapstoneProjectLendingApplicationnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurionpro.lending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectLendingApplicationnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
